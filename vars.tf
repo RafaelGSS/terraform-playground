42b944c979cd58976ef0e5135ab4081b8d9cb0ef
RafaelGSS <rafael.nunu@hotmail.com>
2020-09-01 18:45:02 -0300

    add to run as provides based on tfvars

--- vars.tf
+++ vars.tf
@@ -1,16 +1,3 @@
-variable "example" {
-  type = "string"
-  default = "foo"
-}
-
-variable "example_map" {
-  type = map(string)
-  default = {
-    foo = "bar"
-  }
-}
-
-variable "example_list" {
-  type = list
-  default = [1, 2, 3]
+variable "AWS_REGION" {
+  type = string
 }
